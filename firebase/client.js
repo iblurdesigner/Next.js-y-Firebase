3a0e87e876c0b905aa4936693ba3e2cefba3397f
iblurdesigner <nevomen@gmail.com>
2021-09-12 22:48:27 -0500

    variables de entorno

--- firebase/client.js
+++ firebase/client.js
@@ -1,15 +1,17 @@
 import firebase from "firebase"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA4LOYTg3VOKCWQ9upkUMNyNUEFvoZ_F_s",
-  authDomain: "blurtter-5bc24.firebaseapp.com",
-  databaseURL: "https://blurtter-5bc24-default-rtdb.firebaseio.com",
-  projectId: "blurtter-5bc24",
-  storageBucket: "blurtter-5bc24.appspot.com",
-  messagingSenderId: "340637433065",
-  appId: "1:340637433065:web:8f676f3c37b5f37b954478",
-  measurementId: "G-H07VP93ZBX",
-}
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA4LOYTg3VOKCWQ9upkUMNyNUEFvoZ_F_s",
+//   authDomain: "blurtter-5bc24.firebaseapp.com",
+//   databaseURL: "https://blurtter-5bc24-default-rtdb.firebaseio.com",
+//   projectId: "blurtter-5bc24",
+//   storageBucket: "blurtter-5bc24.appspot.com",
+//   messagingSenderId: "340637433065",
+//   appId: "1:340637433065:web:8f676f3c37b5f37b954478",
+//   measurementId: "G-H07VP93ZBX",
+// }
+
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
 
 !firebase.apps.length && firebase.initializeApp(firebaseConfig)
 
